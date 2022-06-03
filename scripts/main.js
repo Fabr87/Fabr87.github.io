5e17a1d7441368233d72a93931cb64e8c7fce543
qtqqt <shiorurin@gmail.com>
2022-06-03 11:36:40 +1100

    test js stuff

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+﻿function changeImage(img) {
+    var mySrc = document.getElementById("myImage");
+    if (mySrc.getAttribute("src") === 'images/favicon.bmp') {
+        mySrc.setAttribute('src', 'images/Heart.png' );
+    } else {
+        mySrc.setAttribute('src', 'images/favicon.bmp');
+    }
+}
+
+let helloP = document.getElementById("hello");
+
+let newNameButton = document.getElementById('btn');
+
+newNameButton.onclick = () => {
+    setUserName();
+}
+
+function setUserName() {
+    let myName = prompt('Введите имя');
+    localStorage.setItem('name', myName);
+    helloP.textContent = 'Hello, ' + myName + '!';
+}
+
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Hello, ' + storedName + '!'
+}
+
+

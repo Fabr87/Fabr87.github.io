d3cbf22c5f6ec9ec57cf717a1b109384b9ed73db
Fabr87 <pes2012al@mail.ru>
2022-06-08 09:56:37 +1100

    Spoiler fix

--- scripts/spoiler.js
+++ scripts/spoiler.js
@@ -1,11 +1,7 @@
-function open_close(id_spol) {
-var obj = "";
-if (document.getElementById) obj = document.getElementById(id_spol).style;
-else if (document.all) obj = document.all[id_spol];
-else if (document.layers) obj = document.layers[id_spol];
-else return 1;
+$(function () {
 
-if (obj.display == "") obj.display = "none";
-else if (obj.display != "none") obj.display = "none";
-else obj.display = "block";
-}
+    $(".floating-button").click(function () {
+        $(".spoiler").toggle("normal");
+    });
+
+});

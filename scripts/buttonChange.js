42c8d71ba03b4f52d2fc3f429f7d106081a7671f
Fabr87 <pes2012al@mail.ru>
2022-06-08 15:51:28 +1100

    Contact style

---
+++ scripts/buttonChange.js
@@ -0,0 +1,16 @@
+/*btnclck = true;
+function ChangeBgColor(Element) {
+    if (btnclck == true) {
+        Element.style.backgroundColor = '#2ea2e5';
+        Element.style.transform = 'translateY(-7px)';
+        btnclck = false;
+        return true;
+    }
+    else
+    {
+        Element.style.backgroundColor = '';
+        Element.style.transform = '';
+        btnclck = true;
+        return true;
+    }
+        }*/
